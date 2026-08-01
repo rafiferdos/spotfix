733b8246aa259c42ba6c7611d67f11554df54eb9
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-01 22:22:47 +0600

    refactor: comment out unused rewrites function in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: "https://spotfix-api.vercel.app/api/:path*",
+  //     },
+  //   ]
+  // },
+  images: {
+    remotePatterns: [
       {
-        source: "/api/:path*",
-        destination: "https://spotfix-api.vercel.app/api/:path*",
+        protocol: "https",
+        hostname: "**",
       },
-    ]
+    ],
   },
 }
 
