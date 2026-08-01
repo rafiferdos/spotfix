d86ee533e49d2c326876b4e6d3e7cc1b3fdc82e9
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-01 23:24:33 +0600

    refactor: format getServices function for improved readability

--- features/services/api.ts
+++ features/services/api.ts
@@ -1,10 +1,12 @@
 import { axiosInstance } from "@/lib/axios"
 import { ServiceType } from "./types"
 
-export const getServices = async (params?: Record<string, any>): Promise<ServiceType[]> => {
-  const response = await axiosInstance.get("/services", { params });
-  return response.data.data;
-};
+export const getServices = async (
+  params?: Record<string, any>
+): Promise<ServiceType[]> => {
+  const response = await axiosInstance.get("/services", { params })
+  return response.data.data
+}
 export const createService = async (paylaod: Omit<ServiceType, "id">) => {
   const response = await axiosInstance.post("/services", paylaod)
   return response.data.data
