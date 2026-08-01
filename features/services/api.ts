4c12f1519d4e049f31e6d325a44909044911c3d5
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-01 23:24:15 +0600

    feat: update getServices function to accept query parameters

--- features/services/api.ts
+++ features/services/api.ts
@@ -1,11 +1,10 @@
 import { axiosInstance } from "@/lib/axios"
 import { ServiceType } from "./types"
 
-export const getServices = async (): Promise<ServiceType[]> => {
-  const response = await axiosInstance.get("/services")
-  return response.data.data
-}
-
+export const getServices = async (params?: Record<string, any>): Promise<ServiceType[]> => {
+  const response = await axiosInstance.get("/services", { params });
+  return response.data.data;
+};
 export const createService = async (paylaod: Omit<ServiceType, "id">) => {
   const response = await axiosInstance.post("/services", paylaod)
   return response.data.data
