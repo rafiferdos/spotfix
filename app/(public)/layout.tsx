1524ab7c647f07732969261e971000acef976f2a
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-01 21:13:00 +0600

    feat: add layout and navigation components with dropdown functionality

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,47 @@
+import { Geist_Mono, Montserrat, Outfit } from "next/font/google"
+
+import AuthProvider from "@/components/providers/auth-provider"
+import QueryProvider from "@/components/providers/query-provider"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
+import { Toaster } from "sileo"
+import Navbar from "@/components/shadcn-space/blocks/navbar-01/navbar"
+
+const outfitHeading = Outfit({ subsets: ["latin"], variable: "--font-heading" })
+
+const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-sans" })
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        montserrat.variable,
+        outfitHeading.variable
+      )}
+    >
+      <body suppressHydrationWarning>
+        <Toaster position="top-center" theme="system" />
+        <Navbar />
+        <ThemeProvider>
+          <QueryProvider>
+            <AuthProvider>{children}</AuthProvider>
+          </QueryProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
