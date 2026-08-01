cc29e344f7513c91423e1248b562849e53c6c5da
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-02 00:10:30 +0600

    fix: update apiUrl to use environment variable for flexibility and enable async rewrites in next.config

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+  apiUrl: "/api",
   isProduction: process.env.NODE_ENV === "production",
   logoToken: process.env.LOGO_TOKEN,
 } as const
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: "https://spotfix-api.vercel.app/api/:path*",
-  //     },
-  //   ]
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://spotfix-api.vercel.app/api/:path*",
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
